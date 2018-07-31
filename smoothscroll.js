c951d68f55755cd436ad897ae4b7c832420016e4
Chad Tolkien <chad@tolkien.id.au>
2018-07-31 13:14:11 +1000

    removed autowireup, added offset

--- smoothscroll.js
+++ smoothscroll.js
@@ -51,9 +51,10 @@ var position = function(start, end, elapsed, duration) {
 // if the first argument is numeric then scroll to this location
 // if the callback exist, it is called when the scrolling is finished
 // if context is set then scroll that element, else scroll window
-var smoothScroll = function(el, duration, callback, context){
+var smoothScroll = function(el, duration, callback, context, options){
     duration = duration || 500;
     context = context || window;
+    var offset = options.offset || 0;
     var start = context.scrollTop || window.pageYOffset;
 
     if (typeof el === 'number') {
@@ -61,6 +62,8 @@ var smoothScroll = function(el, duration, callback, context){
     } else {
       var end = getTop(el, start);
     }
+
+    end += offset;
 
     var clock = Date.now();
     var requestAnimationFrame = window.requestAnimationFrame ||
@@ -87,32 +90,6 @@ var smoothScroll = function(el, duration, callback, context){
     step();
 }
 
-var linkHandler = function(ev) {
-    if (!ev.defaultPrevented) {
-        ev.preventDefault();
-
-        if (location.hash !== this.hash) window.history.pushState(null, null, this.hash)
-        // using the history api to solve issue #1 - back doesn't work
-        // most browser don't update :target when the history api is used:
-        // THIS IS A BUG FROM THE BROWSERS.
-        // change the scrolling duration in this call
-        var node = document.getElementById(this.hash.substring(1))
-        if (!node) return; // Do not scroll to non-existing node
-
-        smoothScroll(node, 500, function (el) {
-            location.replace('#' + el.id)
-            // this will cause the :target to be activated.
-        });
-    }
-}
-
-// We look for all the internal links in the documents and attach the smoothscroll function
-document.addEventListener("DOMContentLoaded", function () {
-    var internal = document.querySelectorAll('a[href^="#"]:not([href="#"])'), a;
-    for(var i=internal.length; a=internal[--i];){
-        a.addEventListener("click", linkHandler, false);
-    }
-});
 
 // return smoothscroll API
 return smoothScroll;
